e2ca7778fbbbed6ddf907e6a5df5388973084bbd
Ahmed Omar <ahmed42795@gmail.com>
2021-09-02 00:41:26 +0200

    kubernetes cluster done

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=ahmedo42/localproject
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run localproject --image=$dockerpath --port=80 --labels app=localproject
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward localproject 8000:80
 
